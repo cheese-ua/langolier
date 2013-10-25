4ec0ac121838c48e877d48a787cfae90a25b98be
cheese-ua <cheese.ua@gmail.com>
2013-10-25 16:00:32 +0300

    refactor

--- src/main_socket.erl
+++ src/main_socket.erl
@@ -17,8 +17,6 @@
          terminate/2, code_change/3, receive_from_client/1]).
 
 
-
-
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -35,12 +33,14 @@ init(_Args) ->
   accept(SocketServer),
    {ok}.
 
+%% ------------------------------------------------------------------
 accept(SocketServer) ->
   {ok, SocketClient} = gen_tcp:accept(SocketServer),
   gen_server:cast(?SERVER, {accept_socket_client, SocketClient}),
   spawn_link(?SERVER, receive_from_client, [SocketClient]),
   accept(SocketServer).
 
+%% ------------------------------------------------------------------
 receive_from_client(SocketClient) ->
   case gen_tcp:recv(SocketClient, 0) of
     {ok, Bytes} ->
@@ -50,31 +50,36 @@ receive_from_client(SocketClient) ->
       {error, closed}
   end.
 
+%% ------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-
+%% ------------------------------------------------------------------
 handle_cast({receive_from_client, SocketClient, NewBytes}, #state{bytes=Bytes} = State) ->
   logger:info("Receive message: ~w~n", [NewBytes]),
   NewState = State#state{
     bytes=[{SocketClient, NewBytes} | Bytes]
   },
   {noreply, NewState};
+
 handle_cast({start_socket_server, SocketServer}, State) ->
   logger:info("Start server: ~w~n", [SocketServer]),
   NewState = State#state{
     server=SocketServer
   },
   {noreply, NewState};
+
 handle_cast({accept_socket_client, SocketClient}, #state{clients=Clients} = State) ->
   logger:info("Accept client: ~w~n", [SocketClient]),
   NewState = State#state{
    clients=[SocketClient | Clients]
   },
   {noreply, NewState};
+
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+%% ------------------------------------------------------------------
 handle_info(_Info, State) ->
     {noreply, State}.
 
